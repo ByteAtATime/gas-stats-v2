f19eecfb2fc4937521077052ae7727489919b1d1
ByteAtATime <ByteAtATime@proton.me>
2024-06-16 09:50:11 -0700

    feat: add utility to convert wei to eth in token

--- src/lib/tokens/types.ts
+++ src/lib/tokens/types.ts
@@ -1,7 +1,15 @@
-export interface TokenProvider {
-	name: string;
-	symbol: string;
-	decimals: number;
+export abstract class TokenProvider {
+	abstract name: string;
+	abstract symbol: string;
+	abstract decimals: number;
 
-	getPrice(): Promise<number>;
+	abstract getPrice(): Promise<number>;
+
+	numberToUnits(number: bigint): number {
+		const halfDecimals = this.decimals / 2;
+
+		const temp = number / 10n ** BigInt(Math.ceil(halfDecimals));
+
+		return Number(temp) / 10 ** Math.floor(halfDecimals);
+	}
 }
